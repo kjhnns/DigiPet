8b360e7f2a5bcdeec47d090d6c35b5411968238f
Johannes Klumpe <klumpe@live.de>
2015-06-20 18:02:08 +0200

    added Privacy Statement Policy flip

--- assets/lib/app.js
+++ assets/lib/app.js
@@ -6,16 +6,42 @@ var myApp = new Framework7({
 // We need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add view
+// Adding views
+var digiPet = myApp.addView('.view-digipet');
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
 
 
-var digiPet = myApp.addView('.view-digipet');
-
-
-$$('.launchApp').on('click', function () {
+// User Story
+$$('.launchApp').on('click', function() {
     myApp.showTab('.view-digipet');
+});
+
+var redirection = function() {
+    myApp.alert("REDIRECT");
+    // parent.window.location = "http://google.de";
+};
+
+var disclosureRequest = function() {
+    myApp.alert('Are you willing to allow DigiPet to access your GPS Location?', 'Disclosure Request', function() {
+        redirection();
+    });
+};
+
+
+$$('.view.view-digipet').on('show', function() {
+    if (__pps) {
+        disclosureRequest();
+    }
+
+    // show the activities picker
+    myApp.pickerModal('.picker-activies');
+});
+
+$$('.picker-activies .activity-travel').on('click', function() {
+    if (!__pps) {
+        disclosureRequest();
+    }
 });
